52cb82121eef30179741a0c62aa3544fb80aabec
narwa <narwandaru.nara@gmail.com>
2023-09-21 12:12:02 +0700

    clean tree, removed unused overloaded remove methods

--- Minggu-2/src/praktikum/Tree.java
+++ Minggu-2/src/praktikum/Tree.java
@@ -83,8 +83,7 @@ public class Tree<T extends Comparable<T>> {
      *
      * @param key nilai yang akan dihapus
      */
-    public void remove(T key) {
-        System.out.println("removing... " + key);
+    public Node<T> remove(T key) {
         Node<T> parent = this.root;
         Node<T> curr = this.root;
 
@@ -96,11 +95,10 @@ public class Tree<T extends Comparable<T>> {
                 parent = curr;
                 curr = curr.getRight();
             } else {
-                performRemoval(parent, curr);
-                return;
+                return performRemoval(parent, curr);
             }
         }
-        System.out.println("can't find " + key);
+        return null;
     }
 
     /**
@@ -144,121 +142,34 @@ public class Tree<T extends Comparable<T>> {
             }
             return curr;
         }
-        /* Jika current anak kiri kosong */
-        else if (curr.getLeft() == null) {
-            if (parent.getRight() == curr) {
-                parent.setRight(curr.getRight());
-            } else {
-                parent.setLeft(curr.getRight());
-            }
-            return curr;
-        }
-        /* Jika current anak kanan kosong */
-        else if (curr.getRight() == null) {
-            if (parent.getLeft() == curr) {
-                parent.setLeft(curr.getLeft());
-            } else {
-                parent.setRight(curr.getLeft());
-            }
-            return curr;
-        }
-        /* Jika current memiliki anak kanan dan anak kiri */
-        else {
-            Node<T> temp = curr.getRight();
-            Node<T> leftMostParent = curr;
 
-            while (curr.getLeft() != null) {
-                leftMostParent = curr;
-                curr = curr.getLeft();
-            }
-
-            temp.setData(curr.getData());
-            if (leftMostParent == curr) {
-                temp.setRight(curr.getRight());
-            } else {
-                leftMostParent.setLeft(curr.getRight());
-            }
-            return temp;
-        }
-    }
-
-    /* Versi lain */
-    public Node<T> remove(Node<T> root, T key) {
-        Node<T> curr = root;
-        Node<T> parent = null;
-
-        /* Mencari node dengan nilai yang sama selama curr tidak kosong dan kunci belum ditemukan */
-        while (curr != null && key.compareTo(curr.getData()) != 0) {
-            parent = curr;
-            if (key.compareTo(curr.getData()) < 0) {
-                curr = curr.getLeft();
-            } else {
-                curr = curr.getRight();
-            }
-        }
-
-        /* curr dari kondisi sebelumnya tidak menemukan kecocokan dengan kunci manapun */
-        if (curr == null) {
-            return root;
-        }
-
-        /* Mengecek dimana node harus punya tidak lebih dari 1 anak */
         if (curr.getLeft() == null || curr.getRight() == null) {
-            Node<T> replacement;                                // penyimpan node yang akan menggantikan node yg dihapus
-
-            if (curr.getLeft() == null) {                       // JIKA tidak punya anak kiri
-                replacement = curr.getRight();                  // ubah node pengganti dengan anak kanan curr
-            } else {                                            // JIKA tidak punya anak kanan
-                replacement = curr.getLeft();                   // ubah node pengganti dengan anak kiri cur
+            Node<T> child = (curr.getLeft() != null) ?
+                curr.getLeft() :
+                curr.getRight();
+            if (parent.getLeft() == curr) {
+                parent.setLeft(child);
+            } else {
+                parent.setRight(child);
             }
-
-            /* Jika node yang akan dihapus adalah root (root tidak punya parent (parent)) */
-            if (parent == null) {
-                return replacement;
-            }
-
-            if (curr == parent.getLeft()) {                     // JIKA curr (yg akan dihapus) adalah anak kiri
-                parent.setLeft(replacement);                    // atur anak kiri parent dgn replacement
-            } else {                                            // JIKA curr (yg akan dihapus) adalah anak kanan
-                parent.setRight(replacement);                   // atur anak kanan parent (parent) dgn replacement
-            }
+            return curr;
         }
-        /* Node yang akan dihapus memiliki 2 anak */
-        else {
-            Node<T> tempParent = null;
-            Node<T> temp;
 
-            /* Mendapatkan node terkecil yang masih lebih besar (subtree kanan) dari nilai node yang akan dihapus */
-            temp = curr.getRight();
-            while (temp.getLeft() != null) {                    // SELAMA anak kiri temp tidak kosong
-                tempParent = temp;                              // isi tempParent dengan temp
-                temp = temp.getLeft();                          // pindah temp ke anak kirinya
-            }
+        Node<T> temp = curr.getRight();
+        Node<T> leftMostParent = curr;
 
-            /* Mengecek apakah node terkecil memiliki parent bernilai sama dengan yang ingin dihapus */
-            // ini memastikan node terkecil subtree dipindah sebagai anak kiri parent
-            if (tempParent != null) {
-                tempParent.setLeft(temp.getRight());
-            }
-
-            /* Node terkecil merupakan anak kanan langsung dari node dengan nilai yang ingin dihapus (curr) */
-            // ini memastikan anak kanan dari node yang ingin dihapus menjadi anak kanan curr
-            else {
-                curr.setRight(temp.getRight());
-            }
-
-            /* Update the parent of the node to be deleted with the node that replaces it */
-            if (parent == null) {                               // JIKA node yang akan dihapus adalah root
-                root = temp;                                    // ubah root menjadi temp
-            } else if (curr == parent.getLeft()) {              // JIKA node yang akan dihapus adalah anak kiri parent
-                parent.setLeft(temp);                           // atur anak kiri parent menjadi temp
-            } else {                                            // JIKA node yang akan dihapus adalah anak kanan parent
-                parent.setRight(temp);                          // atur anak kanan parent menjadi temp
-            }
-
-            curr.setData(temp.getData());                       // menukar data curr dengan data temp
+        while (curr.getLeft() != null) {
+            leftMostParent = curr;
+            curr = curr.getLeft();
         }
-        return root;
+
+        temp.setData(curr.getData());
+        if (leftMostParent == curr) {
+            temp.setRight(curr.getRight());
+        } else {
+            leftMostParent.setLeft(curr.getRight());
+        }
+        return temp;
     }
 
     /**
