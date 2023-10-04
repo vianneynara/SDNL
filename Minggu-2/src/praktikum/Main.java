f1bc230808adc32c7927ba68e62d0c0f5a54279d
narwa <narwandaru.nara@gmail.com>
2023-10-04 10:31:53 +0700

    Separated methods to test each tree

--- Minggu-2/src/praktikum/Main.java
+++ Minggu-2/src/praktikum/Main.java
@@ -2,7 +2,23 @@ package praktikum;
 
 public class Main {
 
-    public static void main(String[] args) {
+    /**
+     * Sebuah wrapper untuk mencari {@code key} pada sebuah {@link Tree} menggunakan metode {@link Tree#search}.
+     *
+     * @param tree
+     * Tree yang digunakan untuk mencari {@code key}
+     * @param key
+     * dataset atau kunci yang ingin dicari di dalam {@code tree}.
+     * */
+    public static <T extends Comparable<T>> void cari(Tree<T> tree, T key) {
+        System.out.println(                                     // mencetak:
+          (tree.search(key) != null) ?                          // JIKA hasil pencarian tidak kosong
+            "Ditemukan " + key :                                // cetak "Ditemukan " + key
+            "Tidak ditemukan " + key);                          // SELAIN ITU cetak "Tidak ditemukan " + key
+    }
+
+    private static void runBSTTest() {
+
         var tree = new Tree<Integer>();                         // membuat objek Tree ke var. tree
         int[] dataset = {56, 30, 40, 22, 70, 95, 60, 65, 11, 16, 63, 67, 3, 37, 88};
         int[] toHapus = {100, 63, 65, 60, 95, 88, 67, 70, 56, 22, 16, 11, 3, 30, 40, 37};
@@ -29,18 +45,11 @@ public class Main {
         System.out.println("\nTest selesai.");
     }
 
-    /**
-     * Sebuah wrapper untuk mencari {@code key} pada sebuah {@link Tree} menggunakan metode {@link Tree#search}.
-     *
-     * @param tree
-     * Tree yang digunakan untuk mencari {@code key}
-     * @param key
-     * dataset atau kunci yang ingin dicari di dalam {@code tree}.
-     * */
-    public static <T extends Comparable<T>> void cari(Tree<T> tree, T key) {
-        System.out.println(                                     // mencetak:
-          (tree.search(key) != null) ?                          // JIKA hasil pencarian tidak kosong
-            "Ditemukan " + key :                                // cetak "Ditemukan " + key
-            "Tidak ditemukan " + key);                          // SELAIN ITU cetak "Tidak ditemukan " + key
+    private static void runAVLTest() {
+
+    }
+
+    public static void main(String[] args) {
+        runAVLTest();
     }
 }
