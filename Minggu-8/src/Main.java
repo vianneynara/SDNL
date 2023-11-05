f1094605506090cd799fb3f250334ed25222f5ce
narwa <narwandaru.nara@gmail.com>
2023-11-06 02:24:44 +0700

    Improve readability for adding edges

--- Minggu-8/src/Main.java
+++ Minggu-8/src/Main.java
@@ -15,18 +15,18 @@ public class Main {
 		graph.addVertex("I"); // 8
 
 		graph.displayVertices();
-		graph.addEdge(0, 1); // AB
-		graph.addEdge(0, 5); // AF
-		graph.addEdge(0, 8); // AI
-		graph.addEdge(1, 2); // BC
-		graph.addEdge(1, 4); // BE
-		graph.addEdge(2, 3); // CD
-		graph.addEdge(2, 4); // CE
-		graph.addEdge(3, 6); // DG
-		graph.addEdge(3, 7); // DH
-		graph.addEdge(4, 6); // EG
-		graph.addEdge(5, 6); // FG
-		graph.addEdge(6, 4); // GE
+		graph.addEdge("A", "B"); // AB | graph.addEdge(0, 1);
+		graph.addEdge("A", "F"); // AF | graph.addEdge(0, 5);
+		graph.addEdge("A", "I"); // AI | graph.addEdge(0, 8);
+		graph.addEdge("B", "C"); // BC | graph.addEdge(1, 2);
+		graph.addEdge("B", "E"); // BE | graph.addEdge(1, 4);
+		graph.addEdge("C", "D"); // CD | graph.addEdge(2, 3);
+		graph.addEdge("C", "E"); // CE | graph.addEdge(2, 4);
+		graph.addEdge("D", "G"); // DG | graph.addEdge(3, 6);
+		graph.addEdge("D", "H"); // DH | graph.addEdge(3, 7);
+		graph.addEdge("E", "G"); // EG | graph.addEdge(4, 6);
+		graph.addEdge("F", "G"); // FG | graph.addEdge(5, 6);
+		graph.addEdge("G", "E"); // GE | graph.addEdge(6, 4);
 		System.out.println("Adjacency Matrix:");
 		graph.displayMatrix();
 
