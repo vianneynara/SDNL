0e6154d4fa30e19b48bb722328dece59eaad61a2
narwa <narwandaru.nara@gmail.com>
2023-11-13 14:38:52 +0700

    Added weighted graph example

--- Minggu-8/src/Main.java
+++ Minggu-8/src/Main.java
@@ -3,7 +3,7 @@ import praktikum.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Graph graph = new Graph(9);
+		Graph graph = new Graph(10);
 		graph.addVertex("A"); // 0
 		graph.addVertex("B"); // 1
 		graph.addVertex("C"); // 2
@@ -13,27 +13,36 @@ public class Main {
 		graph.addVertex("G"); // 6
 		graph.addVertex("H"); // 7
 		graph.addVertex("I"); // 8
+		graph.addVertex("J"); // 9
 
 		graph.displayVertices();
-		graph.addEdge("A", "B"); // AB | graph.addEdge(0, 1);
-		graph.addEdge("A", "F"); // AF | graph.addEdge(0, 5);
-		graph.addEdge("A", "I"); // AI | graph.addEdge(0, 8);
-		graph.addEdge("B", "C"); // BC | graph.addEdge(1, 2);
-		graph.addEdge("B", "E"); // BE | graph.addEdge(1, 4);
-		graph.addEdge("C", "D"); // CD | graph.addEdge(2, 3);
-		graph.addEdge("C", "E"); // CE | graph.addEdge(2, 4);
-		graph.addEdge("D", "G"); // DG | graph.addEdge(3, 6);
-		graph.addEdge("D", "H"); // DH | graph.addEdge(3, 7);
-		graph.addEdge("E", "G"); // EG | graph.addEdge(4, 6);
-		graph.addEdge("F", "G"); // FG | graph.addEdge(5, 6);
-		graph.addEdge("G", "E"); // GE | graph.addEdge(6, 4);
-		System.out.println("Adjacency Matrix:");
-		graph.displayMatrix();
+		graph.addEdge("A", "B", 5);
+		graph.addEdge("A", "C", 5);
+		graph.addEdge("B", "D", 4);
+		graph.addEdge("B", "E", 9);
+		graph.addEdge("C", "D", 4);
+		graph.addEdge("C", "F", 7);
+		graph.addEdge("D", "E", 8);
+		graph.addEdge("D", "F", 6);
+		graph.addEdge("E", "G", 10);
+		graph.addEdge("E", "H", 8);
+		graph.addEdge("E", "I", 9);
+		graph.addEdge("F", "G", 3);
+		graph.addEdge("G", "H", 6);
+		graph.addEdge("G", "J", 7);
+		graph.addEdge("H", "I", 4);
+		graph.addEdge("H", "J", 6);
+		graph.addEdge("I", "J", 7);
+//		System.out.println("Adjacency Matrix:");
+//		graph.displayMatrix();
+//
+//		System.out.println("Depth First Search:");
+//		graph.depthFirstSearch();
+//
+//		System.out.println("Breadth First Search:");
+//		graph.breadthFirstSearch();
 
-		System.out.println("Depth First Search:");
-		graph.depthFirstSearch();
-
-		System.out.println("Breadth First Search:");
-		graph.breadthFirstSearch();
+		System.out.println("Minimum Spanning Tree:");
+		graph.minimumSpanningTree_Prim();
 	}
 }
