efb51e86c9e4fb52b07c4a97a3042b0888d31208
narwa <narwandaru.nara@gmail.com>
2023-12-03 15:08:02 +0700

    Added Dijkstra traversal test

--- Minggu-8/src/Main.java
+++ Minggu-8/src/Main.java
@@ -2,6 +2,41 @@ import praktikum.*;
 
 public class Main {
 
+	public static void regularBFSDFS() {
+		Graph graph = new Graph(10);
+		graph.addVertex("A"); // 0
+		graph.addVertex("B"); // 1
+		graph.addVertex("C"); // 2
+		graph.addVertex("D"); // 3
+		graph.addVertex("E"); // 4
+		graph.addVertex("F"); // 5
+		graph.addVertex("G"); // 6
+		graph.addVertex("H"); // 7
+		graph.addVertex("I"); // 8
+
+		graph.displayVertices();
+		graph.addEdge("A", "B");
+		graph.addEdge("A", "F");
+		graph.addEdge("A", "I");
+		graph.addEdge("B", "C");
+		graph.addEdge("B", "E");
+		graph.addEdge("C", "D");
+		graph.addEdge("C", "E");
+		graph.addEdge("D", "G");
+		graph.addEdge("D", "H");
+		graph.addEdge("E", "G");
+		graph.addEdge("F", "G");
+		graph.addEdge("G", "E");
+		System.out.println("Adjacency Matrix:");
+		graph.displayMatrix();
+
+		System.out.println("\nDepth First Search:");
+		graph.depthFirstSearch();
+
+		System.out.println("\nBreadth First Search:");
+		graph.breadthFirstSearch();
+	}
+
 	public static void regularMSTs() {
 		Graph graph = new Graph(10);
 		graph.addVertex("A"); // 0
@@ -13,7 +48,7 @@ public class Main {
 		graph.addVertex("G"); // 6
 		graph.addVertex("H"); // 7
 		graph.addVertex("I"); // 8
-		graph.addVertex("J"); // 9
+//		graph.addVertex("J"); // 9
 
 		graph.displayVertices();
 		graph.addEdge("A", "B", 5);
@@ -36,18 +71,49 @@ public class Main {
 		graph.addEdge("I", "J", 7);
 		System.out.println("Adjacency Matrix:");
 		graph.displayMatrix();
-//
-//		System.out.println("\nDepth First Search:");
-//		graph.depthFirstSearch();
 
-//		System.out.println("\nBreadth First Search:");
-//		graph.breadthFirstSearch();
+		System.out.println("\nDepth First Search:");
+		graph.depthFirstSearch();
 
-//		System.out.println("\nMinimum Spanning Tree (PRIM):");
-//		graph.minimumSpanningTree_Prim();
+		System.out.println("\nBreadth First Search:");
+		graph.breadthFirstSearch();
 
-//		System.out.println("\nMinimum Spanning Tree (KRUSKAL):");
-//		graph.minimumSpanningTree_KruskalV1();
+		System.out.println("\nMinimum Spanning Tree (PRIM):");
+		graph.minimumSpanningTree_Prim();
+
+		System.out.println("\nMinimum Spanning Tree (KRUSKAL):");
+		graph.minimumSpanningTree_KruskalV1();
+	}
+
+	public static void testDijkstra() {
+		// make Graph from A to F
+		Graph graph = new Graph(6);
+		graph.addVertex("A"); // 0
+		graph.addVertex("B"); // 1
+		graph.addVertex("C"); // 2
+		graph.addVertex("D"); // 3
+		graph.addVertex("E"); // 4
+		graph.addVertex("F"); // 5
+
+		graph.addDirectedEdge("A", "B", 50);
+		graph.addDirectedEdge("A", "C", 30);
+		graph.addDirectedEdge("A", "D", 100);
+		graph.addDirectedEdge("A", "F", 10);
+		graph.addDirectedEdge("B", "C", 5);
+		graph.addDirectedEdge("C", "B", 5);
+		graph.addDirectedEdge("D", "B", 20);
+		graph.addDirectedEdge("D", "C", 50);
+		graph.addDirectedEdge("E", "D", 15);
+		graph.addDirectedEdge("E", "F", 15);
+		graph.addDirectedEdge("F", "D", 10);
+
+		System.out.println("Adjacency Matrix:");
+		graph.displayMatrix();
+
+		var src = "A";
+		var dst = "D";
+		System.out.printf("\nDjikstra from %s to %s: %n", src, dst);
+		graph.dijkstra(src, dst);
 	}
 
 	public static void topologicalSortTest() {
@@ -70,12 +136,16 @@ public class Main {
 		graph.addDirectedEdge("J5", "J7");
 
 		graph.displayMatrix();
+		System.out.println();
 
-		System.out.println("Topological Sort v1	:");
+		System.out.println("Topological Sort	: ");
 		graph.topologicalSort();
 	}
 
 	public static void main(String[] args) {
-		topologicalSortTest();
+//		regularBFSDFS();
+//		regularMSTs();
+//		topologicalSortTest();
+		testDijkstra();
 	}
 }
